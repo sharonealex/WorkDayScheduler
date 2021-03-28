ceaf54f2031b1c575802c83a60d25b2d8a858493
Sharon <sharonelizalex@gmail.com>
2021-03-28 20:21:44 +1100

    added flow comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@
 //Create and display todays date and day in the specified moment format
 var today = moment().format('dddd, MMM Do YYYY');
 $("#thisDay").html(today);
+
+//Function to get current hour eg.8 am, 9am
+
+//fetch and display saved tasks from local storage.
+
+//Function for saving tasks on clicking button event.
+
+//Function to change background colours based on time hours
+
