6691dea1b7340ce68954c336f5b6870fa9fa049d
Sharon <sharonelizalex@gmail.com>
2021-03-29 12:12:33 +1100

    added function for time tracking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,45 @@
+//initialize and declare variables
+var today = moment().format('dddd, MMM Do YYYY');
+var hourNow = moment().hour();
+var hourBlock;
+
 
 //Create and display todays date and day in the specified moment format
-var today = moment().format('dddd, MMM Do YYYY');
+
 $("#thisDay").html(today);
 
 //Function to get current hour eg.8 am, 9am
+
+function timeTracker(){
+
+    $(".hour").each(function(){
+        
+        //hourBlock = parseInt($(this).text().split(" ")[0]);
+        hourBlock = parseInt($(this).attr("id"));
+
+        //Based on time of the hour, change background colour to inidicate past present future.
+        if(hourBlock < hourNow){
+            $(this).siblings(".description").addClass("past");
+            $(this).siblings(".description").removeClass("present");
+            $(this).siblings(".description").removeClass("future");
+        }
+        else if(hourBlock == hourNow){
+            $(this).siblings(".description").addClass("present");
+            $(this).siblings(".description").removeClass("past");
+            $(this).siblings(".description").removeClass("future");
+        }
+        else {
+            $(this).siblings(".description").addClass("future");
+            $(this).siblings(".description").removeClass("present");
+            $(this).siblings(".description").removeClass("past");
+        }
+    })
+
+}
+
+
+timeTracker();
+
 
 //fetch and display saved tasks from local storage.
 
